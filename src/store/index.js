2c8b7a01ad47ce9f5e5e1ef4780f0a8e61d731ea
Dave Hemmer <dave_hemmer@hotmail.com>
2021-09-09 16:22:51 -0700

    axios setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,33 @@
 import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    nations: [],
+  },
+  getters: {
+    nations(state) {
+      return state.nations;
+    },
+  },
+  mutations: {
+    SET_NATIONS(state, nations) {
+      state.nations = nations;
+    },
+  },
+  actions: {
+    fetchNations({ commit }) {
+      return new Promise((resolve) => {
+        axios
+          .get(
+            "https://datausa.io/api/data?drilldowns=Nation&measures=Population"
+          )
+          .then((response) => {
+            commit("SET_NATIONS", response.data.data);
+            resolve(response.data);
+          });
+      });
+    },
+  },
   modules: {},
 });
