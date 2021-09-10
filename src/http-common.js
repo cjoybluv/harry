f937f134fe73853424e3ed147519fa9e884a7eab
Dave Hemmer <dave_hemmer@hotmail.com>
2021-09-10 06:42:55 -0700

    apollo setup

--- src/http-common.js
+++ src/http-common.js
@@ -1,27 +1,24 @@
 import axios from "axios";
 
-// import {
-//   ApolloClient,
-//   createHttpLink,
-//   InMemoryCache,
-// } from "@apollo/client/core";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
 
-// // HTTP connection to the API
-// const httpLink = createHttpLink({
-//   uri: "http://localhost:3000/graphql",
-// });
-// // Cache implementation
-// const cache = new InMemoryCache();
-// // Create the apollo client
-// const apolloClient = new ApolloClient({
-//   link: httpLink,
-//   cache,
-// });
+const httpLink = createHttpLink({
+  uri: process.env.VUE_APP_GRAPHQL_URI,
+});
+const cache = new InMemoryCache();
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
 
-// export { apolloClient };
+export { apolloClient };
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: process.env.VUE_APP_API_URI,
   headers: {
     "Content-type": "application/json",
   },
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import "prismjs/themes/prism-coy.css";
 import "@/assets/layout/layout.scss";
 import "@/assets/layout/flags/flags.css";
 
-import { createApp, reactive } from "vue";
+import { createApp, h, provide, reactive } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
@@ -15,10 +15,24 @@ import store from "./store";
 import axios from "axios";
 import VueAxios from "vue-axios";
 
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import { apolloClient } from "@/http-common";
+
 import PrimeVue from "primevue/config";
 import { registerPrime } from "./primeComponentReg";
 
-const app = createApp(App).use(store).use(router).use(VueAxios, axios);
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+
+  render: () => h(App),
+});
+
+app.use(store);
+app.use(router);
+app.use(VueAxios, axios);
+
 app.provide("axios", app.config.globalProperties.axios);
 app.use(PrimeVue, { ripple: true });
 app.config.globalProperties.$appState = reactive({ inputStyle: "outlined" });
